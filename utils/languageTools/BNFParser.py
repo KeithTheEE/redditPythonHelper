a524d8d14a0230c67e834de7bd4f111efd8e9ed0
CrakeNotSnowman <kmurrayis@gmail.com>
2019-07-29 09:02:59 -0500

    Adding stashed and empty modules for validating code

---
+++ utils/languageTools/BNFParser.py
@@ -0,0 +1,22 @@
+
+
+
+
+class lc_letter(object):
+    pass
+
+    
+
+class name(object):
+    pass
+
+def code_block():
+    pass
+
+
+class scriptTree(object):
+    pass
+
+
+def rasalgul():
+    pass
---
+++ utils/languageTools/codeLineEval.py
@@ -0,0 +1,14 @@
+
+
+
+
+'''
+I have no idea what this is going to turn into 
+
+this is intended to be a collection of functions to evaluate
+a single line and determine if it follows python syntax on the assumption
+that all varibles have been properly defined elsewhere
+
+
+'''
+
